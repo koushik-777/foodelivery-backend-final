80f78c5406dfaeea8dc94d1247b1dc3a11bfd8cd
koushik murali <koushikaadhy@gmail.com>
2025-03-08 15:42:30 +0530

    Update restaurantController.js

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Restaurant = require('../models/Restaurant'); // Assuming you have a Restaurant model
 
 const menus = {
