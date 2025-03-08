f117e4bca8c725ac987e38691d5ed2cd19d3350d
oafof <koushikaadhy@gmail.com>
2025-03-08 15:15:40 +0530

    Add restaurantController with login, register, and getMenu functions

---
+++ models/Restaurant.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+  restaurantName: {
+    type: String,
+    required: true,
+  },
+  ownerName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
